dc1d70074e4f6deb9afe5e588872d1c18523e0ea
Aydar Sharafiev <aydarsh@gmail.com>
2016-11-03 17:29:48 +0300

    problem 67

---
+++ problem67.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+//find the maximum sum in the triangle
+//cat triangle.txt | ./problem67
+
+const int N=100;
+
+int main(int argc, char **argv)
+{
+	int A[N], B[N];
+	for (int i=0; i<N; i++)	A[i]=B[i]=0;
+	
+	scanf("%d", &A[0]);
+	for(int i=1; i<N; i++){
+		for(int j=0; j<=i; j++) scanf("%d", &B[j]);
+		B[0] += A[0];
+		for(int j=1; j<=i; j++) B[j]=(A[j-1]+B[j] > A[j]+B[j])?A[j-1]+B[j]:A[j]+B[j];
+		for(int j=0; j<=i; j++) A[j]=B[j];
+	}
+	int max=0;
+	for(int i=0; i<N; i++)
+		max=(B[i]>max)?B[i]:max;
+	printf("The maximum sum is %d\n", max);
+	
+	return 0;
+}
+
